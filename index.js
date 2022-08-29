18dfee5a447c02a53719b2193d3bdc95c7bf7950
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-08-28 21:10:29 -0400

    Update index.js
    Added prompt for the user to choose which type of employee they would like to add.

--- index.js
+++ index.js
@@ -7,8 +7,57 @@ const { writeFile, copyFile } = require('./src/generate-team')
 const { Engineer, engQuestions } = require('./lib/Engineer');
 const { Intern, internQuestions } = require('./lib/Intern');
 const { Manager, managerQuestions } = require('./lib/Manager');
+const { inheritInnerComments } = require('@babel/types');
 
 //Hold employee type objects.
-const employeesPromt = []
+const employeesArray = []
+
+const init = () => { employeePrompt() }
+//Asks user which type of Employee they would like to their team.
+const employeePrompt = () => {
+    inquirer.prompt([{
+            type: 'list',
+            name: 'employeeType',
+            message: "What kind of team member would you like to add?",
+            choices: [{
+                    name: 'Engineer',
+                    value: "addEngineer"
+                },
+                {
+                    name: 'Intern',
+                    value: "addIntern"
+                },
+                {
+                    name: 'Manager',
+                    value: "addManager"
+                },
+                {
+                    name: 'DONE',
+                    value: "done"
+                }
+            ]
+        }])
+        .then(answer => {
+            if (answer.employeeType === 'addEngineer') {
+                engQuestions();
+            }
+            if (answer.employeeType === 'addIntern') {
+                internQuestions();
+            };
+            if (answer.employeeType === 'addManager') {
+                managerQuestions();
+            };
+            if (answer.employeeType === 'done') {
+                let html = template(employeesArray)
+                console.log('...');
+
+                writeFile(html);
+
+            }
+
+        })
+}
 
 
+
+init()
